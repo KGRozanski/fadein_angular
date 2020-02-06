8b4332980a68bf92d38a3f30e9be70a8ff818929
kgrozanski <kgrozanski@agora.pl>
2020-02-06 10:39:46 +0100

    v.1.1.2 + Professions chips added

--- src/app/modules/user/components/profile/profile.component.ts
+++ src/app/modules/user/components/profile/profile.component.ts
@@ -1,134 +1,142 @@
-import { Component, ViewChild, AfterViewInit, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { User } from '../../../../core/models/user.model';
 import { UserDataService } from '../../../../core/services/userdata.service';
 import { CropComponent } from '../../../../shared/tools/components/crop/crop.component';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {FormControl} from '@angular/forms';
-import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { COMMA,ENTER } from '@angular/cdk/keycodes';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete } from '@angular/material';
+import { Observable } from 'rxjs';
+import { map,startWith } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
-  animations: [
-    trigger('toggle', [
-      state('true', style({
-        'height': '420px'
-      })),
-      state('false', style({
-        'height': '0'
-      })),
-      transition('*=>true', animate('200ms')),
-      transition('*=>false', animate('300ms'))
-    ])
-  ]
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss'],
+	animations: [
+		trigger('toggle', [
+			state('true', style({
+				'height': '420px'
+			})),
+			state('false', style({
+				'height': '0'
+			})),
+			transition('*=>true', animate('200ms')),
+			transition('*=>false', animate('300ms'))
+		])
+	]
 })
-export class ProfileComponent implements AfterViewInit, OnInit {
-  visible = true;
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  professionCtrl = new FormControl();
-  filteredprofessions: Observable<string[]>;
-  professions: string[] = [];
-  allProfessions: string[] = [];
+export class ProfileComponent implements OnInit {
+	private visible = true;
+	private selectable = true;
+	private removable = true;
+	private addOnBlur = true;
+	private separatorKeysCodes: number[] = [ENTER, COMMA];
+	private professionCtrl = new FormControl();
+	private filteredprofessions: Observable < string[] > ;
+	private professions: string[] = [];
+	private allProfessions: string[] = [];
+	private allProfestionsForRemoving: string[] = [];
 
-  @ViewChild('professionInput') professionInput: ElementRef<HTMLInputElement>;
-  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+	@ViewChild('professionInput') professionInput: ElementRef < HTMLInputElement > ;
+	@ViewChild('auto') matAutocomplete: MatAutocomplete;
+	@ViewChild(CropComponent) crop;
 
-  @ViewChild(CropComponent) crop;
+	ngOnInit() {
+		this.us.getProfessions().subscribe((profs) => {
+			let professions = profs['body'];
+			if (professions) {
+				professions.forEach(element => {
+					this.allProfessions.push(element.name);
+					this.allProfestionsForRemoving.push(element.name);
+				});
+			}
+		})
+	}
 
-  private user: User;
-  private imgData = {
-    image: null
-  };
-  private isCropperShown = false;
+	private user: User;
+	private imgData = {
+		image: null
+	};
+	private isCropperShown = false;
 
-  constructor(private us: UserDataService) {
-    this.us.currentUserData.subscribe((data) => {
-      this.user = data;
-      this.imgData.image = data.avatar;
-    });
-    this.filteredprofessions = this.professionCtrl.valueChanges.pipe(
-      startWith(null),
-      map((profession: string | null) => profession ? this._filter(profession) : this.allProfessions.slice()));
-  }
+	constructor(private us: UserDataService) {
+		this.us.currentUserData.subscribe((data) => {
+			this.user = data;
+			this.imgData.image = data.avatar;
+		});
+		this.filteredprofessions = this.professionCtrl.valueChanges.pipe(
+			startWith(null),
+			map((profession: string | null) => profession ? this._filter(profession) : this.allProfessions.slice()));
+	}
 
 
-  reciveImage($event) {
-    this.imgData.image = $event;
-    this.imgData = this.crop.data;
-  }
+	reciveImage($event) {
+		this.imgData.image = $event;
+		this.imgData = this.crop.data;
+	}
 
-  toggleCrop(isCanceled) {
-    if (isCanceled == true) {
-      this.us.currentUserData.subscribe((data) => {
-        this.user = data;
-        this.imgData.image = data.avatar;
-      });
-    }
-    this.isCropperShown = !this.isCropperShown;
-  }
+	toggleCrop(isCanceled) {
+		if (isCanceled == true) {
+			this.us.currentUserData.subscribe((data) => {
+				this.user = data;
+				this.imgData.image = data.avatar;
+			});
+		}
+		this.isCropperShown = !this.isCropperShown;
+	}
 
-  add(event: MatChipInputEvent): void {
-    // Add profession only when MatAutocomplete is not open
-    // To make sure this does not conflict with OptionSelected Event
-    if (!this.matAutocomplete.isOpen) {
-      const input = event.input;
-      const value = event.value;
+	addProfession(event: MatChipInputEvent): void {
+		// Add profession only when MatAutocomplete is not open
+		// To make sure this does not conflict with OptionSelected Event
+		if (!this.matAutocomplete.isOpen) {
+			const input = event.input;
+			const value = event.value;
+			let findProf = this.allProfessions.find(cur => cur == event.value);
+			if(findProf != undefined) {
+				//Remove profession from list
+				let indexOfProf = this.allProfessions.indexOf(event.value);
+				this.allProfessions.splice(indexOfProf, 1);
+				// Add our profession
+				if ((value || '').trim()) {
+					this.professions.push(value.trim());
+				}
+				// Reset the input value
+				if (input) {
+					input.value = '';
+				}
+				this.professionCtrl.setValue(null);
+			}
+		}
+	}
 
-      // Add our profession
-      if ((value || '').trim()) {
-        this.professions.push(value.trim());
-      }
+	removeProfession(profession: string): void {
+		const index = this.professions.indexOf(profession);
+		if (index >= 0) {
+			this.professions.splice(index, 1);
+			let indexOfProf = this.allProfestionsForRemoving.indexOf(profession);
+			this.allProfessions.splice(indexOfProf, 0, profession);
+			this.filteredprofessions = this.professionCtrl.valueChanges.pipe(
+				startWith(null),
+				map((profession: string | null) => profession ? this._filter(profession) : this.allProfessions.slice()));
+		}
+		
+	}
 
-      // Reset the input value
-      if (input) {
-        input.value = '';
-      }
+	addSelectedProfession(event: MatAutocompleteSelectedEvent): void {
+		this.professions.push(event.option.viewValue);
+		this.professionInput.nativeElement.value = '';
+		this.professionCtrl.setValue(null);
+		let indexOfProf = this.allProfessions.indexOf(event.option.viewValue);
+		this.allProfessions.splice(indexOfProf, 1);
+	}
 
-      this.professionCtrl.setValue(null);
-    }
-  }
 
-  remove(profession: string): void {
-    const index = this.professions.indexOf(profession);
+	private _filter(value: string): string[] {
+		const filterValue = value.toLowerCase();
+		return this.allProfessions.filter(profession => profession.toLowerCase().indexOf(filterValue) === 0);
+	}
 
-    if (index >= 0) {
-      this.professions.splice(index, 1);
-    }
-  }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.professions.push(event.option.viewValue);
-    this.professionInput.nativeElement.value = '';
-    this.professionCtrl.setValue(null);
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.allProfessions.filter(profession => profession.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-  ngAfterViewInit() {
-
-  }
-  ngOnInit() {
-    this.us.getProfessions().subscribe((profs) => {
-      let professions = profs['body'];
-      if(professions) {
-        professions.forEach(element => {
-          this.allProfessions.push(element.name);
-        });
-      }
-      
-    })
-  }
 
 }
