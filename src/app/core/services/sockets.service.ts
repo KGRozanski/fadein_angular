0307f4c7b82d229528b48f0905ce465aff9d5c4e
kgrozanski <kgrozanski@agora.pl>
2020-10-17 18:51:40 +0200

    fix(SOCKETS): removed sockets service code

--- src/app/core/services/sockets.service.ts
+++ src/app/core/services/sockets.service.ts
@@ -13,28 +13,29 @@ import {
 export class SocketsService {
 
     constructor(private us: UserDataService) {
-        this.us.USER_STATE.subscribe((user) => {
-            if (user.username !== undefined && !this.socketConnectionFlag) {
-                this.user = user;
-                this.joinRoom = {
-                    username: this.user.username,
-                    room: 1
-                };
-                this.socketConnectionFlag = true;
-                this.SOCKETurl += this.user.username;
-                this.joinRoom.room = 1;
+        // this.us.USER_STATE.subscribe((user) => {
+        //     if (user.username !== undefined && !this.socketConnectionFlag) {
+        //         this.user = user;
+        //         this.joinRoom = {
+        //             username: this.user.username,
+        //             room: 1
+        //         };
+        //         this.socketConnectionFlag = true;
+        //         this.SOCKETurl += this.user.username;
+        //         this.joinRoom.room = 1;
 
-                console.log(this.SOCKETurl);
-                this.socket = io.connect(this.SOCKETurl);
-                this.connectSocket();
-            }
-        });
+        //         console.log(this.SOCKETurl);
+        //         this.socket = io.connect(this.SOCKETurl);
+        //         this.connectSocket();
+        //     }
+        // });
 
-        setTimeout(() => {
-            this.socket.emit('chatMessage', {
-                addressee: this.user.username,
-                recipient: 'wox',
-                msg: 'dzień dobry :)'
-            });
-        }, 3000);
+        // setTimeout(() => {
+        //     this.socket.emit('chatMessage', {
+        //         addressee: this.user.username,
+        //         recipient: 'wox',
+        //         msg: 'dzień dobry :)'
+        //     });
+        // }, 3000);
     }
+}
